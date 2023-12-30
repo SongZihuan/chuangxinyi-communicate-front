cbb4d03ce2c9b58edbf47474d7899ca446a21e95
SongZihuan <songzihuan@song-zh.com>
2023-12-30 14:09:10 +0800

    feat: 完善backtop

--- components/BackToTop.vue
+++ components/BackToTop.vue
@@ -1,68 +1,9 @@
 <template>
-  <div class="">
-    <div v-if="show" @click="backToTop" class="back-to-top">
-      回顶部
-    </div>
-  </div>
+  <el-backtop :right="40" :bottom="100" />
 </template>
 
 <script setup lang="ts">
-
-let show = ref(false)
-let timer = ref("")
-let speed = ref(10)
-
-const props = defineProps({
-  leaveTop: {
-    type: Number,
-    default: 300
-  }
-})
-
-const getScrollTop = () => {
-  return document.documentElement.scrollTop || document.body.scrollTop
-}
-
-const backToTop = () => {
-  const currentScroll = getScrollTop()
-  if (currentScroll > 0) {
-    window.scrollTo(0, currentScroll - speed.value)
-    // 加速
-    speed.value += 2
-    timer.value = requestAnimationFrame(() => {
-      backToTop()
-    })
-  } else {
-    speed.value = 10
-    cancelAnimationFrame(timer.value)
-  }
-}
-
-onMounted(()=>{
-  window.addEventListener('scroll', () => {
-    show.value = getScrollTop() > props.leaveTop
-  })
-})
-
 </script>
 
 <style scoped lang="scss">
-@import '@/assets/styles/variables';
-.back-to-top {
-  position: fixed;
-  right: 40px;
-  bottom: 80px;
-  padding: 10px;
-  width: 36px;
-  color: #999;
-  text-align: center;
-  line-height: 1.25;
-  border: 1px solid #ddd;
-  background: #fff;
-  cursor: pointer;
-  z-index: 9999;
-  &:hover {
-    background: #f3f3f3;
-  }
-}
 </style>
