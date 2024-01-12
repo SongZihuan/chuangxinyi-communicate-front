54cbfb007ecf223b30d492a1d5f1bacad1f8350d
SongZihuan <songzihuan@song-zh.com>
2024-01-12 15:12:29 +0800

    feat: 修复footer显示问题

--- components/Footer.vue
+++ components/Footer.vue
@@ -10,19 +10,19 @@
           </el-text>
         </div>
         <div class="flex flex-row justify-center items-center my-3">
-          <div class="mx-3" v-if="config?.copyright?.lenght !== 0">
+          <div class="mx-3" v-if="config?.copyright">
             <el-text>
               © {{ appConfig.CopyrightYear as string }} {{ config.copyright }} 版权所有
             </el-text>
           </div>
 
-          <div class="mx-3" v-if="config?.icp?.lenght !== 0" @click="Utils.openTo('https://beian.miit.gov.cn/')">
+          <div class="mx-3" v-if="config?.icp" @click="Utils.openTo('https://beian.miit.gov.cn/')">
             <el-text>
               {{ config.icp }}
             </el-text>
           </div>
 
-          <div class="flex flex-row mx-3 justify-center items-center" v-if="config?.gongan?.lenght !== 0" @click="Utils.openTo('https://beian.mps.gov.cn/#/query/webSearch')">
+          <div class="flex flex-row mx-3 justify-center items-center" v-if="config?.gongan" @click="Utils.openTo('https://beian.mps.gov.cn/#/query/webSearch')">
             <img
               :src="beian" class="w-[20px] h-[20px] mr-[4px]"
               alt="gongan"
