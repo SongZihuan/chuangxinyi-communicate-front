a3f49efe5a93e5b143b6592943e67bcec6942f66
SongZihuan <songzihuan@song-zh.com>
2024-03-11 11:52:31 +0800

    feat: 添加health check

---
+++ pages/_health/_check/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="text-center">
+  系统运行正常...
+</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'base'
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
